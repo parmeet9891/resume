b10295c91f67d7f82ef29ce9a14b80a1065fc4d0
parmeet9891 <parmeet_coder@yahoo.com>
2019-07-20 20:37:43 +0530

    Updates

--- precache-manifest.f3b6de187d8eb22cc6d4f42dc5a8524b.js
+++ precache-manifest.f3b6de187d8eb22cc6d4f42dc5a8524b.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "ea815a8ba94620c49a65b9b8144a80a1",
+    "revision": "f8a054a6c8c0ce0ff04af36d9cd1acf3",
     "url": "/resume/index.html"
   },
   {
-    "revision": "a2c136b979c9febccb36",
+    "revision": "970506210b86f1b5f6d3",
     "url": "/resume/static/css/main.bd4106fa.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/resume/static/js/2.efb86595.chunk.js"
   },
   {
-    "revision": "a2c136b979c9febccb36",
-    "url": "/resume/static/js/main.0ffeff81.chunk.js"
+    "revision": "970506210b86f1b5f6d3",
+    "url": "/resume/static/js/main.23febaec.chunk.js"
   },
   {
     "revision": "699f89d957187d0ac949",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/resume/precache-manifest.9bc08a5bc00aa61b1b2a0fc14e70f1ae.js"
+  "/resume/precache-manifest.f3b6de187d8eb22cc6d4f42dc5a8524b.js"
 );
 
 self.addEventListener('message', (event) => {
