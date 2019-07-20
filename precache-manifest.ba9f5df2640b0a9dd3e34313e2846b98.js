9199098efe4d39c120066bdbf432fd74112fdd16
parmeet9891 <parmeet_coder@yahoo.com>
2019-07-20 20:58:50 +0530

    Updates

---
+++ precache-manifest.ba9f5df2640b0a9dd3e34313e2846b98.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f2d64879064122dbb9235a386422afb0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "915a1a0adec6a2a35da2",
+    "url": "/static/css/main.bd4106fa.chunk.css"
+  },
+  {
+    "revision": "64bdd4e3ac0d55d944f0",
+    "url": "/static/js/2.efb86595.chunk.js"
+  },
+  {
+    "revision": "915a1a0adec6a2a35da2",
+    "url": "/static/js/main.68870cbc.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
