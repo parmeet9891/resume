efa5813746458ab40847f855ad905a1beeeb7af9
parmeet9891 <parmeet_coder@yahoo.com>
2019-07-20 20:20:35 +0530

    Updates

---
+++ precache-manifest.9b97db9b8c274c29b9844f26db5b7b94.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b36ea45b6fa47e526eff2b389dfcd82c",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "0478317881785e5eed0a",
+    "url": "/resume/static/css/main.bd4106fa.chunk.css"
+  },
+  {
+    "revision": "64bdd4e3ac0d55d944f0",
+    "url": "/resume/static/js/2.efb86595.chunk.js"
+  },
+  {
+    "revision": "0478317881785e5eed0a",
+    "url": "/resume/static/js/main.cae2b287.chunk.js"
+  },
+  {
+    "revision": "699f89d957187d0ac949",
+    "url": "/resume/static/js/runtime~main.30522d0f.js"
+  }
+]);
